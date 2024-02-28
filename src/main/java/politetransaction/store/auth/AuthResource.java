16bb3abb2da6b247443614be12ac9725938f614d
FeMCDias <femcdias@gmail.com>
2024-02-28 12:00:58 -0300

    clean up

--- src/main/java/politetransaction/store/auth/AuthResource.java
+++ src/main/java/politetransaction/store/auth/AuthResource.java
@@ -1,18 +1,7 @@
 package politetransaction.store.auth;
-
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import politetransaction.store.account.AccountController;
-import politetransaction.store.auth.CredentialIn;
-import politetransaction.store.auth.LoginOut;
-import politetransaction.store.auth.RegisterIn;
 
 @RestController
 public class AuthResource implements AuthController{
